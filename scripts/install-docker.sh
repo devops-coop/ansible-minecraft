1304884d54c2862f16f3c921ffb101afc79ec24d
Ben Webber <benjamin.webber@gmail.com>
2016-03-07 23:41:25 -0500

    Fix version number

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,9 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-apt-cache madison docker-engine
-
-apt-get -o Dpkg::Options::="--force-confnew" install -y docker-engine=${DOCKER_VERSION}
+apt-get -o Dpkg::Options::="--force-confnew" install -y docker-engine="${DOCKER_VERSION}"
 
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose

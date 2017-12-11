2d87e328ce563ba92671fc2ae328e95f2bfb6f4a
Ben Webber <benjamin.webber@gmail.com>
2017-12-11 15:41:02 +0000

    Fix Docker package name

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-apt-get install --force-yes -y docker-engine
+apt-get install --force-yes -y docker-ce
 
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose

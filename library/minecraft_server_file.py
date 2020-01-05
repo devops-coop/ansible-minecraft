f6504d5e80c7b02b38eda8387de59f710cc0508f
nolte <nolte07@gmail.com>
2020-01-05 10:17:33 +0100

    merge bugfix (#55)
    * Update .travis.yml
    
    * Fixed python code to be python3 compatible. Fix compatibility for latest Ansible 2.9.2 (#54)
    
    Co-authored-by: Oleksandr Poliatykin <poliatykin@gmail.com>

--- library/minecraft_server_file.py
+++ library/minecraft_server_file.py
@@ -149,7 +149,7 @@ class ServerProperties(ServerFile):
         # Convert all values to strings.  The default booleans in
         # server.properties are represented by "true" and "false" instead of
         # Python's "True" and "False", so convert them explicitly.
-        for name, value in properties.iteritems():
+        for name, value in properties.items():
             properties[name] = (
                 str(value).lower() if isinstance(value, bool) else str(value)
             )
@@ -169,7 +169,7 @@ class ServerProperties(ServerFile):
                 else:
                     self.newlines.append(line)
 
-        for name, value in properties.iteritems():
+        for name, value in properties.items():
             self.newlines.append("{}={}\n".format(name, value))
             self._content_changed = True
 
